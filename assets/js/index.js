7139d8865f016b759358b9dcbac83b6685512b23
Maksym <praniknumberone@gmail.com>
2026-02-26 04:32:31 +0100

    Done Exercice 1,create and minimal-stylized section

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+//EXERCICE1
+
+const section = document.createElement("section");
+section.id="root";
+section.textContent = "Text for example work";
+section.style.color="green";
+document.body.append(section);  
